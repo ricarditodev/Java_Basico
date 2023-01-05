8d042ad9f4a7db9fc32d177531154624fcd6b889
ricarditodev <ricardodavid012@gmail.com>
2023-01-05 15:27:40 -0400

    if-else

--- src/com/company/conceptos/IfElse.java
+++ src/com/company/conceptos/IfElse.java
@@ -2,12 +2,14 @@ package com.company.conceptos;
 
 public class IfElse {
     public static void main(String[] args) {
-        boolean check = 5 < 10;
+        boolean check = 5 > 10;
 
         if (check) {
             System.out.println("Cierto");
-        } else {
+        } else if (!check){
             System.out.println("Falso");
+        } else {
+            System.out.printf("Algo");
         }
     }
 }
