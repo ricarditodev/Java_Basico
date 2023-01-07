288067ed16677fee38ae47fb763c318758171216
ricarditodev <ricardodavid012@gmail.com>
2023-01-06 21:17:27 -0400

    regfactoring

--- src/com/company/funciones/Tarea.java
+++ src/com/company/funciones/Tarea.java
@@ -1,14 +1,26 @@
 package com.company.funciones;
 
+import java.util.Scanner;
+
 public class Tarea {
     public static void main(String[] args) {
-        var price = 220.25;
-        var iva = 0.16;
-        var priceTotal = getIVA(price, iva);
-        System.out.println(priceTotal);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce el IVA");
+        int IVA = scanner.nextInt();
+        double ivaCompuesto = getIVA(IVA);
+
+        System.out.println("Introduce un precio");
+        var price = scanner.nextDouble();
+        var totalPrice = getPrice(price, ivaCompuesto);
+        System.out.println("El precio total de tu compra es " + totalPrice);
     }
 
-    static double getIVA(double price, double iva) {
-        return price * 0.16d + price;
+    static double getIVA(int iva) {
+        return iva / 100d;
+    }
+
+    static double getPrice(double price, double iva) {
+        return price * iva + price;
     }
 }
