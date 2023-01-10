240124338efcacac7ff79d1b8e8757d4f3deee9a
ricarditodev <ricardodavid012@gmail.com>
2023-01-10 00:59:09 -0400

    fix

--- src/com/company/tiposDeDatosAvanzados/Mapas.java
+++ src/com/company/tiposDeDatosAvanzados/Mapas.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Mapas {
     public static void main(String[] args) {
-        HashMap<String, Integer> mapa = new HashMap<String, Integer>();
+        HashMap<String, Integer> mapa = new HashMap<>();
 
         mapa.put("A", 1);
         mapa.put("B", 2);
@@ -21,7 +21,7 @@ public class Mapas {
         //tanto la clave como el valor se guardan en la variable temporal del bucle
         //para obtener la clave usamos el metodo getKey() mientras estamos iterando
         //para obtener el valor usamos el metodo getValue() mientras estamos iterando
-        for (Map.Entry elemento : mapa.entrySet()) {
+        for (Map.Entry<String, Integer> elemento : mapa.entrySet()) {
             System.out.println("Key: " + elemento.getKey());
             System.out.println("Value: " + elemento.getValue());
         }
